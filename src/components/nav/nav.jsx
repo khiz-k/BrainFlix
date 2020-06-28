1bc82317f85b5665a5d72d4ba24452b120c94800
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-27 21:03:09 -0700

    Sprint 2 nav, don't need state so made it prop

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -1,52 +1,28 @@
 import React from 'react';
 import './nav.scss';
-import siteLogo from '../../assets/Logo/Logo-brainflix.svg';
-import userImage from '../../assets/Images/Mohan-muruge.jpg';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
-const LogoComponent = ({logo, changeLogo}) => {
+export default function Nav(props) {
   return (
-    <>
-      <img src={logo} alt="BrainFlix Logo" className="nav-bar__logo-image" onChange={changeLogo} />
-    </>
-  )
-}
-
-const UserImageComponent = ({image, changeImage}) => {
-  return (
-    <>
-      <img src={image} alt="User" className="user-image" onChange={changeImage} />
-    </>
-  )
-}
-
-export default class Nav extends React.Component {
-  state = {
-    logo: siteLogo,
-    image: userImage
-  };
-  changeImage = event => this.setState({image: event.target.value})
-  render() {
-    return (
-      <section class="nav-bar">
-        <div className="nav-bar__logo">
-          <a href="../../../public/index.html" className="nav-bar__anchor">
-            <LogoComponent logo={this.state.logo} changeLogo={() => this.changeLogo} />
-          </a>
-        </div>
-        <nav class="nav-bar__main">
-        <ul className="nav-bar__list">
-          <li className="nav-bar__list-item">
-            <form className="nav-bar__form">
-              <input type="text" name="search" placeholder="Search" className="nav-bar__search" />
-            </form>
-          </li>
-          <li className="nav-bar__list-item user-upload">
-            <button typeof="submit" name="uploadVideo" className="upload-button" id="upload-button" value="upload">Upload</button>
-            <UserImageComponent image={this.state.image} changeImage={() => this.changeImage} />
-          </li>
-        </ul>
-        </nav>
-      </section>
-    )
-  }
+    <section className="nav-bar">
+      <div className="nav-bar__logo">
+        <Link to="/home" className="nav-bar__anchor">
+          <img src={props.logo} alt="BrainFlix Logo" className="nav-bar__logo-image" />
+        </Link>
+      </div>
+      <nav className="nav-bar__main">
+      <ul className="nav-bar__list">
+        <li className="nav-bar__list-item">
+          <form className="nav-bar__form">
+            <input type="text" name="search" placeholder="Search" className="nav-bar__search" />
+          </form>
+        </li>
+        <li className="nav-bar__list-item user-upload">
+        <Link to="/upload" className="upload-button__link"><button name="uploadVideo" className="upload-button" id="upload-button" value="upload">Upload</button></Link>
+          <img src={props.image} alt="User" className="user-image" />
+        </li>
+      </ul>
+      </nav>
+    </section>
+  );
 }
