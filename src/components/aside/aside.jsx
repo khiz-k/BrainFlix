3c92193ffddcdcc50d999c6c720e769e587c0091
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-28 05:28:36 -0700

    Update for routing in APP

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -1,21 +1,22 @@
 import React, { Component } from 'react';
 import './aside.scss';
 import axios from 'axios';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 const url = "https://project-2-api.herokuapp.com/videos?api_key="; 
 const urlMain = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key="; 
 let API_KEY = "7741224a-2544-4acd-945c-a52d003ff057";
 
-const VideoList = ({videos, changeVid}) => {
+const VideoList = ({videos}) => {
   return (
     <>
-      {videos.map(video => <li key={video.id} className="next-video__list-item" onClick={() => changeVid(video.id)}>
-        <a href="./" className="next-video__item-anchor">
-          <img src={video.image} className="next-video__item-image" alt="List Video"/>
+      {videos.map(video => <li key={video.id} className="next-video__list-item">
+        <Link to={"/video/"+video.id} className="next-video__item-anchor">
+          <img src={video.image} className="next-video__item-image" alt={`Video ${video.id}`}/>
           <div className="next-video__item-text">
             <h5 className="next-video__item-title">{video.title}</h5><h5 className="next-video__item-creator">{video.channel}</h5>
           </div>
-        </a>
+        </Link>
       </li>)}    
     </>
   )
