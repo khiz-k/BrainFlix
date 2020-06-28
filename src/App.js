d78e845491dfec2cf5f250d9b9ed1f55dfa66bf7
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-27 20:56:01 -0700

    Sprint 2 First Router Commit, home/upload routes established
    Yet to Do:
    
    landing page needs to be /home
    
    Clicking on a video in the list must go to its own page and its own set of likes, views, author, description, comments, etc.
    ***  Use componentDidUpdate lifecycle method and match.params from to determine when to update the main video data  ***

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
+import axios from "axios";
 import './App.scss';
+// import components
 import Nav from './components/nav/nav';
 import Hero from './components/hero/hero';
 import Article from './components/article/article';
 import Comments from './components/comments/comments';
 import Aside from './components/aside/aside';
+import Upload from './components/vidUpload/upload';
+// import assets
+import brainflixLogo from './assets/Logo/Logo-brainflix.svg';
+import userImage from './assets/Images/Mohan-muruge.jpg';
 
-const App = () => {
-  return (
-    <div className="App">
-      <Nav />
-      <Hero />
-      <main>
-        <div className="not-aside">
-          <Article />
-          <Comments />
-        </div>
-        <Aside />
-      </main>
-    </div>
-  );
+class App extends React.Component  {
+  state = {
+    siteLogo: brainflixLogo,
+    userIcon: userImage
+  }
+  render () {
+    return (
+      <BrowserRouter>
+      <div className="App">
+        <header>
+          <Nav logo={this.state.siteLogo} image={this.state.userIcon} />
+        </header>
+        {/* <Switch> */}
+          <Route path="/home" component={Hero} />
+          <main>
+              <div className="not-aside">
+              <Route path="/upload" component={Upload}/>
+                <Route path="/home"> 
+                  <Article />
+                  <Comments />
+                </Route> 
+              </div>
+              <Route path="/home" component={Aside} />
+          </main>
+        {/* </Switch> */}
+      </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
