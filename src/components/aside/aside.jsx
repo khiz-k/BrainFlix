a70f765e94bb8704b0bd2fa3bb071aaf47ea837d
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-27 20:58:06 -0700

    Prepped video list for routing

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -1,106 +1,47 @@
 import React, { Component } from 'react';
-import './aside.scss'
-// import image from '../../assets/Images/video-list-.jpg';
-// import title from '';
-// import creator from '';
+import './aside.scss';
+import axios from 'axios';
 
-const ImageComponent = ({image, changeImage}) => {
+const url = "https://project-2-api.herokuapp.com/videos?api_key="; 
+const urlMain = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key="; 
+let API_KEY = "7741224a-2544-4acd-945c-a52d003ff057";
+
+const VideoList = ({videos, changeVid}) => {
   return (
     <>
-      <img src={image} className="item-image" alt="List Video" onChange={changeImage} />
-    </>
-  )
-}
-
-const TextComponent = ({title, creator, changeTitle, changeCreator}) => {
-  return (
-    <>
-     <h5 className="item-title" onChange={changeTitle}>{title}</h5><h5 className="item-creator" onChange={changeCreator}>{creator}</h5>
+      {videos.map(video => <li key={video.id} className="next-video__list-item" onClick={() => changeVid(video.id)}>
+        <a href="./" className="next-video__item-anchor">
+          <img src={video.image} className="next-video__item-image" alt="List Video"/>
+          <div className="next-video__item-text">
+            <h5 className="next-video__item-title">{video.title}</h5><h5 className="next-video__item-creator">{video.channel}</h5>
+          </div>
+        </a>
+      </li>)}    
     </>
   )
 }
 
 export default class Aside extends Component {
   state = {
-    image: 'placeholder',
-    title: 'placeholder',
-    creator: 'placeholder'
+    videos: [],
+    mainVidID : {}
   };
-  changeImage = event => this.setState({image: event.target.value})
-  changeTitle = event => this.setState({title: event.target.value})
-  changeCreator = event => this.setState({creator: event.target.value})
+  componentDidMount() {
+    axios.get(url + API_KEY).then(
+      res => this.setState({videos: res.data})
+    )
+  }
+  getMainVidID() {
+    axios.get(urlMain + API_KEY).then(
+      res => this.setState({mainVidID: res.data.id})
+    )
+  }
   render() {
     return (
       <aside className="next-video">
         <h5 className="next-video__title">Next Video</h5>
         <ul className="next-video__list">
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-
-          {/* map first list item and populate via data rather than typing code 9 times */}
-          
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
-          <li className="list-item" /* onClick={(event)=>{props.clickHandler(event, props.title)}} */>
-            <a href="./" className="item-anchor">
-              <ImageComponent image={this.state.image} changeImage={() => this.changeImage}/>
-              <div className="item-text">
-                <TextComponent title={this.state.title} creator={this.state.creator} changeTitle={() => this.changeTitle} changeCreator={() => this.changeCreator} />
-              </div>
-            </a>
-          </li>
+          <VideoList videos={this.state.videos} changeVid={this.changeVid} />
         </ul>
       </aside>
     )
