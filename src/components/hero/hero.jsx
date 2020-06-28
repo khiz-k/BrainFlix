a62edd8d81b0223abfb63ba14f3cd13572466e30
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-28 16:36:50 -0700

    Sprint 2 Hero update, props param

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './hero.scss';
 
 export default function Hero (props) {
+  // const heroID = props.match.params.id;
   return (
     <>
       <section className="hero">
-        <video key={props.id} className="hero-video" poster={props.image} controls>
+        <video key={props.match && props.match.params.id} className="hero-video" poster={props.image} controls>
           <source src={props.source} type="video/mp4" className="video-source"/>
         </video>
       </section>
