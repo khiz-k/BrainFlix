e6118d48da4f624981347a05bfc540d13e21d848
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-27 20:57:11 -0700

    Add files via upload

--- src/components/article/article.jsx
+++ src/components/article/article.jsx
@@ -1,76 +1,80 @@
 import React, { Component } from 'react';
 import './article.scss';
-// import title from '';
-// import name from '';
-// import timestamp from '';
+import axios from 'axios';
 import viewsIcon from '../../assets/Icons/SVG/Icon-views.svg';
-// import views from '';
 import likesIcon from '../../assets/Icons/SVG/Icon-likes.svg';
-// import likes from '';
-// import text from '';
 
-const TitleComponent = ({title, changeTitle}) => {
+const url = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key="; 
+let API_KEY = "7741224a-2544-4acd-945c-a52d003ff057";
+
+const DescriptionComponent = ({description, viewsIcon, likesIcon, changeViews, increaseLikes}) => {
+  let descriptionDate = `${description.timestamp}`; 
+  const timeAgo = (date) => {
+
+    let seconds = Math.floor((new Date() - date) / 1000);
+
+    let interval = Math.floor(seconds / 31536000);
+
+    if (interval > 1) {
+    return interval + " years ago";
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+    return interval + " months ago";
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+    return interval + " days ago";
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+    return interval + " hours ago";
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+    return interval + " minutes ago";
+    }
+    return Math.floor(seconds) + " seconds ago";
+
+    // update time every min
+    setTimeout(timeAgo, 60000);
+};
+let dateFormatted = timeAgo(descriptionDate);
   return (
     <>
-      <h3 className="description__title" onChange={changeTitle}>{title}</h3>
-    </>
-  )
-}
-
-const SubTitleComponent = ({name, changeName, timestamp, changeTimestamp}) => {
-  return (
-    <>
-      <h5 className="description__name" onChange={changeName}>{name}</h5><h5 className="description__timestamp" onChange={changeTimestamp}>{timestamp}</h5>
-    </>
-  )
-}
-
-const StatsComponent = ({viewsIcon, views, changeViews, likesIcon, likes, changeLikes}) => {
-  return (
-    <>
-      <img src={viewsIcon} alt="Views Icon" class="views-icon" /><h5 className="views" onChange={changeViews}>{views}</h5><img src={likesIcon} alt="Likes Icon" class="likes-icon" /><h5 className="likes" onChange={changeLikes}>{likes}</h5> 
-    </>
-  )
-}
-
-const ParagraphComponent = ({text, changeText}) => {
-  return (
-    <>
-      <p className="description__paragraph" onChange={changeText}>{text}</p>
+      <div key={description.id} className="description">
+        <h3 className="description__title" >{description.title}</h3>
+        <div className="description__top">
+          <div className="description__sub-title">
+          <h5 className="description__name" >{description.channel}</h5><h5 className="description__timestamp" >{dateFormatted}</h5>
+          </div>
+          <div className="description__stats">
+          <img src={viewsIcon} alt="Views Icon" className="views-icon" /><h5 className="views" >{description.views}</h5><img src={likesIcon} alt="Likes Icon" className="likes-icon" onClick={increaseLikes}/><h5 className="likes">{description.likes}</h5>
+          </div>
+        </div>    
+        <p className="description__paragraph">{description.description}</p>
+      </div>
     </>
   )
 }
 
 export default class Article extends Component {
   state = {
-    title: 'placeholder',
-    name: 'placeholder',
-    timestamp: 'placeholder',
+    mainVidArticle: [],
     viewsIcon: viewsIcon,
-    views: 'placeholder',
     likesIcon: likesIcon,
-    likes: 'placeholder',
-    text: 'placeholder'
   };
-  changeTitle = event => this.setState({title: event.target.value})
-  changeName = event => this.setState({name: event.target.value})
-  changeTimestamp = event => this.setState({timestamp: event.target.value})
+  componentDidMount() {
+    axios.get(url + API_KEY).then(
+      res => this.setState({mainVidArticle: res.data})
+    )
+  }
   changeViews = event => this.setState({views: event.target.value})
-  changeLikes = event => this.setState({likes: event.target.value})
-  changeText = event => this.setState({text: event.target.value})
+  increaseLikes = event => this.setState({likes: event.target.value+1})
   render() {
     return (
       <article className="description">
-        <TitleComponent title={this.state.title} changeTitle={() => this.changeTitle} />
-        <div className="description__top">
-          <div className="description__sub-title">
-          <SubTitleComponent name={this.state.name} timestamp={this.state.timestamp} changeName={() => this.changeName} changeTimestamp={() => this.changeTimestamp} />
-          </div>
-          <div className="description__stats">
-          <StatsComponent viewsIcon={this.state.viewsIcon} views={this.state.views} likesIcon={this.state.likesIcon} likes={this.state.likes} changeViews={() => this.changeViews} changeLikes={() => this.changeLikes} />
-          </div>
-        </div>    
-        <ParagraphComponent text={this.state.text} changeText={() => this.changeText} />
+        <DescriptionComponent description={this.state.mainVidArticle} viewsIcon={this.state.viewsIcon} likesIcon={this.state.likesIcon} changeDescription={this.changeDescription} onclick={this.state.increaseLikes}/>
       </article>
     )
   }
