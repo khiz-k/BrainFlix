02df508559391d6cb78b7a81c07d5311bb088422
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-28 16:38:28 -0700

    aside overhaul for dynamic compatability

--- src/components/aside/aside.jsx
+++ src/components/aside/aside.jsx
@@ -7,10 +7,13 @@ const url = "https://project-2-api.herokuapp.com/videos?api_key=";
 const urlMain = "https://project-2-api.herokuapp.com/videos/1af0jruup5gu?api_key="; 
 let API_KEY = "7741224a-2544-4acd-945c-a52d003ff057";
 
-const VideoList = ({videos}) => {
+const VideoList = (props) => {
+  // does props.match exist, if it does set to &&
+  let currentVidID = props.match && props.match.params.id;
+  let filteredVideos = props.videos.filter(video => video !== currentVidID );
   return (
     <>
-      {videos.map(video => <li key={video.id} className="next-video__list-item">
+      {filteredVideos.map(video => <li key={video.id} className="next-video__list-item">
         <Link to={"/video/"+video.id} className="next-video__item-anchor">
           <img src={video.image} className="next-video__item-image" alt={`Video ${video.id}`}/>
           <div className="next-video__item-text">
@@ -25,24 +28,22 @@ const VideoList = ({videos}) => {
 export default class Aside extends Component {
   state = {
     videos: [],
-    mainVidID : {}
+    currentVideo: {id: '1af0jruup5gu'}
   };
   componentDidMount() {
     axios.get(url + API_KEY).then(
       res => this.setState({videos: res.data})
     )
-  }
-  getMainVidID() {
-    axios.get(urlMain + API_KEY).then(
-      res => this.setState({mainVidID: res.data.id})
-    )
+    // axios.get(urlMain + API_KEY).then(
+    //   res => this.setState({currentVideo: res.data})
+    //   )
   }
   render() {
     return (
       <aside className="next-video">
         <h5 className="next-video__title">Next Video</h5>
         <ul className="next-video__list">
-          <VideoList videos={this.state.videos} changeVid={this.changeVid} />
+          <VideoList id={this.state.currentVideo.id} videos={this.state.videos} changeVid={this.changeVid} />
         </ul>
       </aside>
     )
