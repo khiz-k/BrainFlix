a9a02fe94992e866942ebacab0b52df4d012a42c
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-06-29 20:08:22 -0700

    sprint 2 nav final commit

--- src/components/nav/nav.jsx
+++ src/components/nav/nav.jsx
@@ -4,25 +4,27 @@ import { Link } from "react-router-dom";
 
 export default function Nav(props) {
   return (
-    <section className="nav-bar">
-      <div className="nav-bar__logo">
-        <Link to="/" className="nav-bar__anchor">
-          <img src={props.logo} alt="BrainFlix Logo" className="nav-bar__logo-image" />
-        </Link>
-      </div>
-      <nav className="nav-bar__main">
-      <ul className="nav-bar__list">
-        <li className="nav-bar__list-item">
-          <form className="nav-bar__form">
-            <input type="text" name="search" placeholder="Search" className="nav-bar__search" />
-          </form>
-        </li>
-        <li className="nav-bar__list-item user-upload">
-        <Link to="/upload" className="upload-button__link"><button name="uploadVideo" className="upload-button" id="upload-button" value="upload">Upload</button></Link>
-          <img src={props.image} alt="User" className="user-image" />
-        </li>
-      </ul>
-      </nav>
-    </section>
+    <header>
+      <section className="nav-bar">
+        <div className="nav-bar__logo">
+          <Link to="/" className="nav-bar__anchor">
+            <img src={props.logo} alt="BrainFlix Logo" className="nav-bar__logo-image" />
+          </Link>
+        </div>
+        <nav className="nav-bar__main">
+        <ul className="nav-bar__list">
+          <li className="nav-bar__list-item">
+            <form className="nav-bar__form">
+              <input type="text" name="search" placeholder="Search" className="nav-bar__search" />
+            </form>
+          </li>
+          <li className="nav-bar__list-item user-upload">
+          <Link to="/upload" className="upload-button__link"><button name="uploadVideo" className="upload-button" id="upload-button" value="upload">Upload</button></Link>
+            <img src={props.image} alt="User" className="user-image" />
+          </li>
+        </ul>
+        </nav>
+      </section>
+    </header>
   );
 }
