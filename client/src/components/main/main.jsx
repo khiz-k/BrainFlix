819ee1ae5c2657321db6f4a68d9f7c2a1721a86d
khizark96 <58965282+khizark96@users.noreply.github.com>
2020-07-05 19:04:23 -0700

    Sprint 3 main final commit, reload functionality complete!

--- client/src/components/main/main.jsx
+++ client/src/components/main/main.jsx
@@ -12,11 +12,6 @@ import UserImage from '../../assets/Images/Mohan-muruge.jpg';
 import ViewsIcon from '../../assets/Icons/SVG/Icon-views.svg';
 import LikesIcon from '../../assets/Icons/SVG/Icon-likes.svg';
 import DefaultCommentImage from "../../assets/Images/default-image.jpg";
-// API INFO
-let videoID = "1af0jruup5gu";
-const myServerVideosURL = "http://localhost:3000/videos";
-const myServerDefaultMainVidURL = "http://localhost:3000/videos/"+videoID;
-
 
 class Main extends React.Component {
   state = {
@@ -28,12 +23,14 @@ class Main extends React.Component {
     mainVid: {},
     videos: [],
     comments: [],
-    commentsTitleCount: 3 + " comments", // comments.length replaces 3
+    commentsTitleCount: `${3} comments`, 
   }
   componentDidMount() {
+    let videoID = "1af0jruup5gu";
+    const myServerVideosURL = "http://localhost:3000/videos";
+    const myServerDefaultMainVidURL = "http://localhost:3000/videos/"+videoID;
     axios.get(myServerVideosURL).then(
       res => this.setState({ videos: res.data })
-        // setTimeout(() => window.scrollTo(0, 0), 100)
     )
     .catch(err => {
       console.log(err);
@@ -45,24 +42,14 @@ class Main extends React.Component {
     .catch(err => {
       console.log(err);
     })
-    if (this.props.match.params.id == this.props.match.params.id) {
-      axios.get(myServerDefaultMainVidURL).then( 
-        res => { this.setState({ mainVid: res.data }); }
-      )
-      .catch(err => {
-        console.log(err);
-      })
-    
-    } else {      
-      let newVideoID = this.props.match.params.id;
-      const myServerNewMainVidURL = "http://localhost:3000/videos/"+newVideoID;
-      axios.get(myServerNewMainVidURL).then( 
-        res => {this.setState({mainVid: res.data}); }
-      )
-      .catch(err => {
-        console.log(err);
-      })
-    }
+    let newVideoID = this.props.match.params.id;
+    const myServerNewMainVidURL = "http://localhost:3000/videos/"+newVideoID;
+    axios.get(myServerNewMainVidURL).then( 
+      res => {this.setState({mainVid: res.data}); }
+    )
+    .catch(err => {
+      console.log(err);
+    })
   }
   componentDidUpdate(prevProps, prevState) {
     if(this.props.match.params.id !== prevProps.match.params.id) {
@@ -75,25 +62,6 @@ class Main extends React.Component {
         console.log(err);}
       )
     }
-    //else
-      // axios.post(videosURL + API_KEY).then(
-      //   res => {this.setState({ videos: res.data }); }
-      // )
-      // .catch(err => {
-      //   console.log(err);}
-      // )
-      // postComments = (userName, userComment) => {
-      //   axios.post(url + API_KEY, {
-      //     name: userName,
-      //     comment: userComment
-      //     })
-      //     .then(res => {
-      //     console.log(res.data)
-      //     })
-      //     .catch(err => {
-      //       console.log(err);}
-      //     )
-      // };
   }
   render() {
     return (
